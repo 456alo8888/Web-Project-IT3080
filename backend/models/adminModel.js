8d0cf54323f47463f9b8eb2bc290df2beeacd484
ChouxAlpaca <trannguyenchien.2510@gmail.com>
2024-10-08 21:58:35 +0700

    adminModel.js

--- backend/models/adminModel.js
+++ backend/models/adminModel.js
@@ -1,33 +1,22 @@
-//mySQL table
-CREATE TABLE residents (
+import mysql2 from 'mysql2/promise'
+
+//Table
+CREATE TABLE admins (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    room VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255) NOT NULL,
-    gender VARCHAR(10) NOT NULL,
-    age INT NOT NULL CHECK (age >= 18),
-    phone VARCHAR(20) NOT NULL UNIQUE,
-    cccd VARCHAR(20) NOT NULL UNIQUE,
-    image VARCHAR(255) NOT NULL,
-    numMember INT NOT NULL,
-    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    updateFeeAuthority BOOLEAN NOT NULL,
+    createFeeAuthority BOOLEAN NOT NULL,
+    updateResidentAuthority BOOLEAN NOT NULL,
+    isRoot BOOLEAN DEFAULT FALSE
 );
 
 //Querry
-const createResident = async (connection, residentData) => {
-    const { room, name, gender, age, phone, cccd, image, numMember } = residentData;
-    const query = INSERT INTO residents (room, name, gender, age, phone, cccd, image, numMember) 
-                   VALUES (?, ?, ?, ?, ?, ?, ?, ?);
-    
-    try {
-        await connection.execute(query, [room, name, gender, age, phone, cccd, image, numMember]);
-        console.log("Resident created successfully");
-    } catch (error) {
-        if (error.code === 'ER_DUP_ENTRY') {
-            console.error("Error: Duplicate entry for room, phone, or CCCD");
-        } else {
-            console.error('Error creating resident:', error);
-        }
-        throw error;
-    }
+const createAdmin = async (connection, adminData) => {
+    const { username, password, updateFeeAuthority, createFeeAuthority, updateResidentAuthority, isRoot } = adminData;
+    const query = `INSERT INTO admins (username, password, updateFeeAuthority, createFeeAuthority, updateResidentAuthority, isRoot)
+                   VALUES (?, ?, ?, ?, ?, ?)`;
+    await connection.execute(query, [username, password, updateFeeAuthority, createFeeAuthority, updateResidentAuthority, isRoot]);
 };
+
+export { createTableQuery, createAdmin };
