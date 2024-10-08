12026aeb039c5bbe58fa2c95db974f817214417b
ChouxAlpaca <trannguyenchien.2510@gmail.com>
2024-10-08 21:28:25 +0700

    mySQL.js

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,3 +1,4 @@
+/*
 import mongoose from "mongoose";
 
 const connectDB = async () => {
@@ -10,5 +11,26 @@ const connectDB = async () => {
     await mongoose.connect(`${process.env.MONGODB_URI}/NMPM`)
 
 }
+
+export default connectDB;
+*/
+
+import mysql from "mysql2/promise";
+
+const connectDB = async () => {
+    try{
+        const connect = await mysql.createConnection({
+            host: process.env.MYSQL_HOST,
+            user: process.env.MYSQL_USER,
+            password: process.env.MYSQL_PASSWORD,
+            database: 'NMPM' 
+        });
+    console.log("MySQL DB Connected");
+        return connection;
+    } catch (error) {
+        console.error('Error connecting to MySQL:', error);
+        throw error;
+    }
+};
 
 export default connectDB;
