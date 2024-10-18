d2e38a5968d87a5fcbbb67ae3ada052d21cd9892
ChouxAlpaca <trannguyenchien.2510@gmail.com>
2024-10-18 20:36:09 +0700

    historyModel.js

--- backend/models/historyModel.js
+++ backend/models/historyModel.js
@@ -1,46 +1,41 @@
-import mysql from 'mysql2/promise';
+import { DataTypes } from 'sequelize';
+import sequelize from 'backend/config/mySQL.js';
+import Fee from 'backend/models/feeModel.js';
 
-//Table
-const createTableQuery = `
-CREATE TABLE IF NOT EXISTS history (
-   id INT AUTO_INCREMENT PRIMARY KEY,
-   feeId INT NOT NULL,
-   feeName VARCHAR(255) NOT NULL,
-   feeCost DECIMAL(10, 2) NOT NULL,
-   room VARCHAR(255) NOT NULL,
-   roomPayed DECIMAL(10, 2) NOT NULL,
-   username VARCHAR(255) NOT NULL,
-   updateAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-`;
-
-const createHistoryTable = async () => {
-   try {
-       const connection = await pool.getConnection();
-       await connection.query(createTableQuery);
-       console.log("History table created successfully");
-       connection.release();
-   } catch (error) {
-       console.error('Error creating history table:', error);
-       throw error;
-   }
-};
-
-//Querry
-const insertHistoryRecord = async (historyData) => {
-   const { feeId, feeName, feeCost, room, roomPayed, username } = historyData;
-   const query = `INSERT INTO history (feeId, feeName, feeCost, room, roomPayed, username) 
-                  VALUES (?, ?, ?, ?, ?, ?)`;
-
-   try {
-       const connection = await pool.getConnection();
-       await connection.query(query, [feeId, feeName, feeCost, room, roomPayed, username]);
-       console.log("History record inserted successfully");
-       connection.release();
-   } catch (error) {
-       console.error('Error inserting history record:', error);
-       throw error;
-   }
-};
-
-export { createHistoryTable, insertHistoryRecord };
+const historySchema = sequelize.define('historySchema', {
+    feeID: {
+        type: DataTypes.INTEGER,
+        allowNull : false,
+        unique: true,
+        references: {
+            model: fee,
+            key: 'id',
+        }
+    },
+    feeName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    feeCost: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+    },
+    room: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    roomPayed: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    updateAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.now,
+    },
+}),
+    export default historySchema;
+    
