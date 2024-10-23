71492a3ee6232c8a4892565369db9ea3e7983405
Hieu PC_VP <phunghieu5c@gmail.com>
2024-10-23 22:46:28 +0700

    sua Admin model su dung sequelize

--- backend/models/historyModel.js
+++ backend/models/historyModel.js
@@ -1,16 +1,19 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-const historySchema = new mongoose.Schema({
-    feeId: {type: mongoose.Schema.Types.ObjectId, ref: 'fee', required: true},
-    feeName: {type: String, required: true},
-    feeCost: {type: Number, required: true},
-    room: {type: String, required: true},
-    roomPayed: {type: Number, required: true},
-    username: {type: String, required: true},
-    updateAt: {type: Date, default: Date.now},
-})
+// const historySchema = new mongoose.Schema({
+//     feeId: {type: mongoose.Schema.Types.ObjectId, ref: 'fee', required: true},
+//     feeName: {type: String, required: true},
+//     feeCost: {type: Number, required: true},
+//     room: {type: String, required: true},
+//     roomPayed: {type: Number, required: true},
+//     username: {type: String, required: true},
+//     updateAt: {type: Date, default: Date.now},
+// })
 
-const historyModel = mongoose.models.history || mongoose.model('history', historySchema)
+// const historyModel = mongoose.models.history || mongoose.model('history', historySchema)
 
 
-export default historyModel
+// export default historyModel
+
+
+
--- index.js
+++ index.js
@@ -1,58 +1,68 @@
-import bcrypt from "bcrypt";
-import dotenv from "dotenv";
-import express from "express";
-import bodyParser from "body-parser";
-import path from "path";
-import connection from "./database.js";
+// import bcrypt from "bcrypt";
+// import dotenv from "dotenv";
+// import express from "express";
+// import bodyParser from "body-parser";
+// import path from "path";
+// import connection from "./database.js";
 
-if (dotenv.config().error) throw new Error("Failed to initialize dotenv");
+// const mysql = require("mysql");
+// const db = mysql.createConnection({
+//   user : "root", 
+//   host:"localhost",
+//   password : "password",
+//   database:"webapp"
+// });
 
-const PORT = process.env.PORT;
-const app = express();
-const db = connection();
+// if (dotenv.config().error) throw new Error("Failed to initialize dotenv");
 
-app.use(bodyParser.json());
+// // const PORT = process.env.PORT;
+// // const app = express();
+// // const db = connection();
 
-// Serve static files in the 'public' directory
-app.use(express.static(path.join(import.meta.dirname, "public")));
+// app.use(bodyParser.json());
 
-// Route to add a user through the API
-app.post("/users", async (req, res) => {
-  const { username, password } = req.body;
-  const saltRounds = 10;
-  let hashedPassword = null;
-  try {
-    hashedPassword = await bcrypt.hash(password, saltRounds);
-  } catch (error) {
-    res.status(500).send(`Password hashing failed: ${error}`);
-    return;
-  }
-  const query = "INSERT INTO users (username, password) VALUES (?, ?)";
+// // Serve static files in the 'public' directory
+// app.use(express.static(path.join(import.meta.dirname, "public")));
 
-  db.query(query, [username, hashedPassword], (err) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send("Error adding user");
-      return;
-    }
-    res.status(200).send("User added successfully");
-  });
-});
+// // Route to add a user through the API
+// app.post("/users", async (req, res) => {
+//   const { username, password } = req.body;
+//   const saltRounds = 10;
+//   let hashedPassword = null;
+//   try {
+//     hashedPassword = await bcrypt.hash(password, saltRounds);
+//   } catch (error) {
+//     res.status(500).send(`Password hashing failed: ${error}`);
+//     return;
+//   }
+//   const query = "INSERT INTO users (username, password) VALUES (?, ?)";
 
-// Route to get the list of users
-app.get("/users", (req, res) => {
-  const query = "SELECT * FROM users";
+//   db.query(query, [username, hashedPassword], (err) => {
+//     if (err) {
+//       console.error(err);
+//       res.status(500).send("Error adding user");
+//       return;
+//     }
+//     res.status(200).send("User added successfully");
+//   });
+// });
 
-  db.query(query, (err, results) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send("Error fetching users");
-      return;
-    }
-    res.status(200).json(results);
-  });
-});
+// // Route to get the list of users
+// app.get("/users", (req, res) => {
+//   const query = "SELECT * FROM users";
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+//   db.query(query, (err, results) => {
+//     if (err) {
+//       console.error(err);
+//       res.status(500).send("Error fetching users");
+//       return;
+//     }
+//     res.status(200).json(results);
+//   });
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
+
