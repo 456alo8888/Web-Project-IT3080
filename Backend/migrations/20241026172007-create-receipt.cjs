05d897ae201cea5468c2cd4fe91aa2efccf8751d
Ryu204 <nguyenanhbao2356@gmail.com>
2024-11-27 11:16:12 +0700

    Fix: migration files

--- Backend/migrations/20241026172007-create-receipt.cjs
+++ Backend/migrations/20241026172007-create-receipt.cjs
@@ -1,6 +1,11 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
+  /**
+   * @param {import('sequelize').QueryInterface} queryInterface
+   * @param {typeof import('sequelize')} Sequelize
+   * @returns {Promise<void>}
+   */
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('receipts', {
       id: {
@@ -11,28 +16,90 @@ module.exports = {
       },
       admin_id: {
         type: Sequelize.INTEGER,
-        // references: {
-        //   model: 'Admins',
-        //   key: 'id'
-        // }
+        references: {
+          model: 'admins',
+          key: 'id'
+        },
+        allowNull: true,
+        onDelete: 'SET NULL'
       },
       resident_id: {
         type: Sequelize.INTEGER,
-        // references: {
-        //   model: 'Residents',
-        //   key: 'id'
-        // }
+        references: {
+          model: 'residents',
+          key: 'id'
+        },
+        allowNull: true,
+        onDelete: 'SET NULL'
       },
-      money_amount: {
+      value: {
         type: Sequelize.FLOAT,
         allowNull: false
       },
       bill_id: {
         type: Sequelize.INTEGER,
-        // references: {
-        //   model: 'Bills',
-        //   key: 'id'
-        // }
+        references: {
+          model: 'bills',
+          key: 'id'
+        },
+        allowNull: false,
+        onDelete: 'RESTRICT'
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+    await queryInterface.createTable('donation_receipts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      admin_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'admins',
+          key: 'id'
+        },
+        allowNull: true,
+        onDelete: 'SET NULL'
+      },
+      resident_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'residents',
+          key: 'id'
+        },
+        allowNull: true,
+        onDelete: 'SET NULL'
+      },
+      room_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'rooms',
+          key: 'id'
+        },
+        allowNull: false,
+        onDelete: 'CASCADE'
+      },
+      value: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+      },
+      fee_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'residents',
+          key: 'id'
+        },
+        allowNull: true,
+        onDelete: 'CASCADE'
       },
       created_at: {
         allowNull: false,
@@ -45,6 +112,7 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('donation_receipts');
     await queryInterface.dropTable('receipts');
   }
 };
