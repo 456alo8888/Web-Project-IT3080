311ad6c84b62c5e429ed6fd12490e76e23718860
Ryu204 <nguyenanhbao2356@gmail.com>
2024-12-23 21:00:37 +0700

    Feat: return vehicle count on room list

--- Backend/controllers/residentController.js
+++ Backend/controllers/residentController.js
@@ -1,6 +1,6 @@
 import { v2 as cloudinary } from "cloudinary"
 import db from "../models/index.js"
-const { Resident, Room, RoomType } = db
+const { Resident, Room, RoomType, Vehicle } = db
 
 function isNumeric(str) {
     return /^\d+$/.test(str); // Checks if the string contains only digits
@@ -241,7 +241,11 @@ const roomList = async (req, res) => {
                 {
                     model: Resident,
                     attributes: ['id']
-                }
+                },
+                {
+                    model: Vehicle,
+                    attributes: ['id'],
+                },
             ]
         });
         // Format the response
@@ -251,6 +255,7 @@ const roomList = async (req, res) => {
                 id: room.id,
                 name: room.roomName,
                 residentCount: room.Residents?.length ?? 0,
+                vehicleCount: room.Vehicles?.length ?? 0,
                 headResidentName: room.headResident?.name ?? null,
                 typeId: room.typeId
             };
